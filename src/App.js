b77972fad77ff09fdb78ec547f1b61791e6bd281
Dheeraj <bagwandheeraj20@gmail.com>
2023-08-28 23:42:44 +0530

    Commited

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Top from "./Components/Top/Top";
+import Mid from "./Components/Mid/Mid";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Top />
+    <Mid />
+    </>
   );
 }
 
---
+++ src/Components/Top/Top.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './top.css'
+
+export default function Top() {
+  return (
+    <>
+    <div className='head'>
+        <div className='brand'>W-App</div>
+    </div>
+    </>
+  )
+}
